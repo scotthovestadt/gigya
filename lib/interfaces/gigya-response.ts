220f03e2c7c998355dd95a061b8c8f5005ab4d22
scotthovestadt <scott.hovestadt@gmail.com>
2017-02-18 18:34:50 -0800

    Add callId and other response keys to base response.

--- lib/interfaces/gigya-response.ts
+++ lib/interfaces/gigya-response.ts
@@ -1,4 +1,8 @@
 export interface GigyaResponse {
+    callId: string;
+    statusCode: number;
+    statusReason: string;
+    time: string;
     errorCode: number;
     errorMessage?: string;
     errorDetails?: string;
