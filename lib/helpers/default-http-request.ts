34ebfa78d48b70f66ab3a61f808b5edb4302b823
Gil Schmidt <gil@gigya-inc.com>
2019-05-24 13:17:55 +0300

    adding option for verbose HTTP logging by environment variable

--- lib/helpers/default-http-request.ts
+++ lib/helpers/default-http-request.ts
@@ -5,6 +5,8 @@ import fs = require('fs');
 import path = require('path');
 
 let certificate: string;
+let verboseHttpError = process.env['GIGYA_VERBOSE_HTTP_LOGGIN'];
+
 function getCertificate(): string {
     if (!certificate) {
         certificate = fs.readFileSync(path.join(__dirname, '../../../assets/cacert.pem')).toString();
@@ -12,10 +14,18 @@ function getCertificate(): string {
     return certificate;
 }
 
+function log(msg : any) : void {
+    if (verboseHttpError) {
+        console.log(msg);
+    }    
+}
+
 /**
  * Make HTTP request to Gigya.
  */
 export const httpRequest: ProxyHttpRequest = <R>(endpoint: string, host: string, requestParams: any) => {
+    let start = Date.now();
+
     return new Promise<GigyaResponse & R>((resolve, reject) => {
         const uri = `https://${host}/${endpoint}`;
         request.post(uri, {
@@ -23,12 +33,15 @@ export const httpRequest: ProxyHttpRequest = <R>(endpoint: string, host: string,
             form: requestParams,
             ca: getCertificate()
         }, (error, response, body) => {
+            log(`request to ${uri} took ${(new Date().getTime() - start) / 1000} seconds`);
             if (error) {
+                log(error);
                 reject(error);
             }
             try {
                 resolve(JSON.parse(body));
             } catch (ex) {
+                log(`failed to parse response body from request to ${uri}\n${body}`);
                 reject(ex);
             }
         });
